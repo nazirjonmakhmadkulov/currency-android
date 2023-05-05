632d5944a62f6d1ceb975bf3be27440f01e187b7
nazirjon <nazirjon-1997@mail.ru>
2023-05-05 21:38:56 +0300

    code review

--- app/src/main/java/com/developer/valyutaapp/ui/widget/WidgetFragment.kt
+++ app/src/main/java/com/developer/valyutaapp/ui/widget/WidgetFragment.kt
@@ -14,10 +14,13 @@ import com.developer.valyutaapp.core.base.Item
 import com.developer.valyutaapp.databinding.FragmentWidgetBinding
 import com.developer.valyutaapp.domain.entities.Valute
 import com.developer.valyutaapp.ui.adapter.ValCursAdapter
+import com.developer.valyutaapp.utils.Utils
 import com.developer.valyutaapp.utils.launchAndCollectIn
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import io.paperdb.Paper
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
+import java.text.DecimalFormat
 
 class WidgetFragment : Fragment(R.layout.fragment_widget) {
     private val viewBinding by viewBinding(FragmentWidgetBinding::bind)
@@ -60,6 +63,22 @@ class WidgetFragment : Fragment(R.layout.fragment_widget) {
     private fun getAllValuteSuccess(valutes: List<Valute>) {
         Timber.d("list == $valuteList")
         this.valutes = valutes.toMutableList()
+    }
+
+    private fun setWidgetData(){
+        val decimalFormat = DecimalFormat("#.####")
+        val decimal = decimalFormat.format(viewBinding.tvValue.text.toString().toDouble())
+        val value = if (viewBinding.tvNominal.text.toString().length < 3) {
+            decimalFormat.format(viewBinding.tvNominal.text.toString().toDouble())
+        } else {
+            viewBinding.tvNominal.text.toString()
+        }
+        Paper.init(requireContext())
+        Paper.book().write("charcode", viewBinding.name1.text)
+        Paper.book().write("charcode2", viewBinding.name2.text)
+        Paper.book().write("nominal", value)
+        Paper.book().write("value", decimal.toString())
+        Paper.book().write("dat", Utils.getDate())
     }
 
     private fun dialogValutes() {
