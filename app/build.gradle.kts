21719f35ca62ee30c166726d22d9379efc203503
nazirjon mahmadkulov <nazirjonmahmadkulov@MacBook-Pro-nazirjon-2.local>
2023-07-03 16:36:31 +0300

    crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -99,4 +101,8 @@ dependencies {
     implementation("com.yandex.android:mobileads:5.9.0")
     implementation("com.yandex.ads.mediation:mobileads-admob:21.3.0.0")
     implementation("com.google.android.gms:play-services-ads:21.3.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:30.3.1"))
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
