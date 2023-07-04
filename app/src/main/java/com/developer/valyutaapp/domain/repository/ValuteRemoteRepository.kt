5c4b7bdbeb4c78ba7a568e0e03b480c3aaf2a525
nazirjon mahmadkulov <nazirjonmahmadkulov@MacBook-Pro-nazirjon-2.local>
2023-07-04 10:22:43 +0300

    clean up

--- app/src/main/java/com/developer/valyutaapp/domain/repository/ValuteRemoteRepository.kt
+++ app/src/main/java/com/developer/valyutaapp/domain/repository/ValuteRemoteRepository.kt
@@ -3,7 +3,6 @@ package com.developer.valyutaapp.domain.repository
 import com.developer.valyutaapp.core.common.Result
 import com.developer.valyutaapp.domain.entities.ValCurs
 import com.developer.valyutaapp.domain.entities.ValHistory
-import retrofit2.http.Query
 
 interface ValuteRemoteRepository {
     suspend fun getAllValutes(date: String, exp: String): Result<ValCurs>
