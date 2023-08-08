71d3afa32c8b8cba809c4229258a80b41ffdc9f6
nazirjon <nazirjon-1997@mail.ru>
2023-08-08 11:51:59 +0300

    version 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.developer.valyutaapp"
         minSdk = 21
         targetSdk = 34
-        versionCode = 11
-        versionName = "2.5.1"
+        versionCode = 12
+        versionName = "2.5.2"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
