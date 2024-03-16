2745eb8e6ee4240ff9f680411d60704ba26c98f1
nazirjon mahmadkulov <nazirjonmahmadkulov@MacBook-Pro-nazirjon.local>
2024-03-16 20:58:00 +0300

    update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,42 +39,42 @@ android {
     }
 }
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.20")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.23")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.multidex:multidex:2.0.1")
 
-    implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.8")
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.9")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     // Logging:
     api("com.jakewharton.timber:timber:5.0.1")
 
     //Fragment
-    implementation("androidx.fragment:fragment-ktx:1.5.6")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 
     // Kotlin + coroutines
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
     implementation("ru.gildor.coroutines:kotlin-coroutines-retrofit:1.1.0")
 
     // Room
-    implementation("androidx.room:room-ktx:2.5.1")
-    implementation("androidx.room:room-runtime:2.5.1")
-    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -83,20 +83,20 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
 
     // Koin main features for Android
-    implementation("io.insert-koin:koin-android:3.3.3")
+    implementation("io.insert-koin:koin-android:3.5.3")
     // Jetpack WorkManager
-    implementation("io.insert-koin:koin-androidx-workmanager:3.3.3")
+    implementation("io.insert-koin:koin-androidx-workmanager:3.5.3")
 
     //Okhttp logging
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.7")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.7")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
 
     //Widget
     implementation("io.github.pilgr:paperdb:2.7.2")
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
-    implementation("com.yandex.android:mobileads:5.7.0")
-    implementation("com.yandex.ads.mediation:mobileads-admob:21.3.0.0")
-    implementation("com.google.android.gms:play-services-ads:21.3.0")
+    implementation("com.yandex.android:mobileads:6.4.1")
+    implementation("com.yandex.ads.mediation:mobileads-admob:22.1.0.0")
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
 }
